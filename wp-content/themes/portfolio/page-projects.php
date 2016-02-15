144633372eb80c751d5942147ace2f497c7c32f4
Corey Margulis <coreymargulis@gmail.com>
2016-02-14 22:26:27 -0600

    initial setup

---
+++ wp-content/themes/portfolio/page-projects.php
@@ -0,0 +1,195 @@
+<?php
+/*
+ Template Name: Projects Homepage
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="intro">
+	<h1>I am interested in how we can incorporate technology into our lives in meaningful ways, blending the physical and digital worlds to make technology feel more natural and human.</h1>
+	<h2>I am currently accepting project requests. Send me a <a href="mailto:coreymargulis@gmail.com">message</a>.</h2>
+</div>
+
+<div class="menu-wrap">
+	<nav>
+		<body>
+				<li>
+					<a id="truth-labs" href="truth-labs">
+						<div class="project-title">Truth Labs</div>
+						<div class="project-tagline">Turning complexity into clarity</div>
+					</a>
+				</li>
+				<li>
+					<a id="saic" href="saic-edu">
+						<div class="project-title">saic.edu</div>
+						<div class="project-tagline">Making the site responsive and improving the user experience for department pages</div>
+					</a>
+				</li>
+				<li>
+					<a id="campus-signage" href="campus-signage">
+						<div class="project-title">Campus Signage</div>
+						<div class="project-tagline">A flexible signage system for the School of the Art Institute of Chicago’s campus</div>
+					</a>
+				</li>
+			<li>
+				<a id="pool-party" href="pool-party">
+					<div class="project-title">Pool Party</div>
+					<div class="project-tagline">Related videos reimagined</div>
+				</a>
+			</li>
+			<li>
+				<a id="nichols-tower" href="nichols-tower">
+					<div class="project-title">Nichols Tower</div>
+					<div class="project-tagline">A website for a new, collaborative tower in Chicago’s Lawndale neighborhood</div>
+				</a>
+			</li>
+			<!-- <li>
+				<a id="pastel-artist" href="pastel-artist">
+					<div class="project-title">Pastel Artist</div>
+					<div class="project-tagline">A revamped website for an internationally renowned pastel artist</div>
+				</a>
+			</li> -->
+			<li>
+				<a id="whats-fresh" href="whats-fresh">
+					<div class="project-title">What’s Fresh</div>
+					<div class="project-tagline">Two ways to find out what produce is in season</div>
+				</a>
+			</li>
+			<li>
+				<a id="commencement" href="commencement">
+					<div class="project-title">Commencement</div>
+					<div class="project-tagline">A dynamic identity for SAIC’s graduation ceremony</div>
+				</a>
+			</li>
+ 		</body>
+	</nav>
+
+<footer class="copyright">
+	© <?php echo date('Y'); ?> Corey Margulis
+</footer>
+
+	<div id="background"></div>
+
+	<script type="text/javascript">
+    $(document).ready(function() {
+	    $("#whats-fresh").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 54); ?>');
+	          //$("#background").css('visibility', 'visible');
+	    }, function() {
+	        // if you want to remove background
+	        // on mouse out then uncomment below line
+	        $('body').css('background-color', '#efefef');
+	        // $("#background").css('visibility', 'hidden');
+	    })
+	    $("#commencement").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 49); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+	    $("#pool-party").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 35); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+	    $("#pastel-artist").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 59); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+	    $("#sbodylivan-galleries").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 62); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+	    $("#conversations").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 117); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+	    $("#campus-signage").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 364); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+			$("#saic").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 595); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+			$("#nichols-tower").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 692); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+			$("#truth-labs").hover(function() {
+	        $('body').css('background-color', '<?php the_field('title-color', 793); ?>');
+	    }, function() {
+	        $('body').css('background-color', '#efefef');
+	    })
+			// $("body a").hover(function() {
+	    //     $('li').css('background-color', '<?php the_field('title-color', 364); ?>');
+	    // }, function() {
+	    //     $('li').css('background-color', '#efefef');
+	    // })
+		});
+	</script>
+
+</div>
+
+
+<!-- <div class="profile-link-wrapper">
+	<a class="profile-link" id="trigger-overlay" href="#">About</a>
+</div>
+
+<div class="overlay overlay-slidedown">
+	<div class="overlay-close-container">
+		<a class="overlay-close" href="#">Close</a>
+	</div>
+	<div class="profile">
+		<?php the_field("profile") ?>
+	</div>
+</div> -->
+
+<!-- <script>
+	(function() {
+	  var triggerBttn = document.getElementById( 'trigger-overlay' ),
+	    overlay = document.querySelector( 'div.overlay' ),
+	    closeBttn = overlay.querySelector( 'a.overlay-close' );
+	    transEndEventNames = {
+	      'WebkitTransition': 'webkitTransitionEnd',
+	      'MozTransition': 'transitionend',
+	      'OTransition': 'oTransitionEnd',
+	      'msTransition': 'MSTransitionEnd',
+	      'transition': 'transitionend'
+	    },
+	    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+	    support = { transitions : Modernizr.csstransitions };
+
+	  function toggleOverlay() {
+	    if( classie.has( overlay, 'open' ) ) {
+	      classie.remove( overlay, 'open' );
+	      classie.add( overlay, 'close' );
+	      var onEndTransitionFn = function( ev ) {
+	        if( support.transitions ) {
+	          if( ev.propertyName !== 'visibility' ) return;
+	          this.removeEventListener( transEndEventName, onEndTransitionFn );
+	        }
+	        classie.remove( overlay, 'close' );
+	      };
+	      if( support.transitions ) {
+	        overlay.addEventListener( transEndEventName, onEndTransitionFn );
+	      }
+	      else {
+	        onEndTransitionFn();
+	      }
+	    }
+	    else if( !classie.has( overlay, 'close' ) ) {
+	      classie.add( overlay, 'open' );
+	    }
+	  }
+
+	  triggerBttn.addEventListener( 'click', toggleOverlay );
+	  closeBttn.addEventListener( 'click', toggleOverlay );
+	})();
+</script> -->
---
+++ wp-content/themes/portfolio/single.php
@@ -0,0 +1,245 @@
+<?php get_header(); ?>
+
+<div id="colorblock" style="background-color:<?php the_field('title-color'); ?>"></div>
+
+<div id="content">
+
+	<?php $bg = get_field( 'hero-image' ); ?>
+	<div class="hero-image-container">
+		<div class="hero-image" style="background-image: url(<?=$bg?>)"></div>
+		<div class="hero-image-caption"><?php the_field('hero-image-caption'); ?></div>
+	</div>
+
+	<div id="inner-content" class="wrap cf">
+
+		<div id="main" class="m-all t-all d-all cf" role="main">
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+          <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
+
+                <header>
+
+                  <h1 class="entry-title" itemprop="headline" style="color:<?php the_field('title-color'); ?>"><?php the_title(); ?></h1>
+				  <h2><?php the_field('tagline'); ?></h2>
+
+			    </header>
+
+                  <?php
+
+					// check if the repeater field has rows of data
+					if( have_rows('content') ) :
+
+					 	// loop through the rows of data
+					    while ( have_rows('content') ) : the_row();
+
+					        // background color change
+
+
+							if( get_sub_field('bkgd-color-change') ) : ?>
+
+							    <section class="entry-content alt" itemprop="articleBody">
+
+							<?php else : ?>
+
+							    <section class="entry-content" itemprop="articleBody">
+
+							<?php endif; ?>
+
+					        <?php // check if the flexible content field has rows of data
+							if( have_rows('section') ):
+
+							     // loop through the rows of data
+							    while ( have_rows('section') ) : the_row();
+
+							        if( get_row_layout() == 'text' ):
+
+							        	the_sub_field('text-field');
+
+							        elseif( get_row_layout() == 'section-title' ): ?>
+
+												<?php if( get_sub_field('project-section-title') ) : ?>
+							        		<h3 class="project"><?php the_sub_field('section-title'); ?></h3>
+												<?php else: ?>
+													<h3><?php the_sub_field('section-title'); ?></h3>
+												<?php endif; ?>
+
+							        <?php elseif( get_row_layout() == 'image' ): ?>
+
+												<?php if(get_sub_field('layout') == "inset-left") : ?>
+													<div class="inset-container">
+														<div class="media inset left">
+															<img src="<?php the_sub_field("image"); ?>" alt="" class="<?php if( get_sub_field('drop-shadow')){ ?>drop-shadow<?php } ?>"/>
+															<div class="caption"><?php the_sub_field("image-caption"); ?></div>
+														</div>
+														<div class="inset-text">
+															<?php if(get_sub_field('section-title')): ?><h3><?php the_sub_field('section-title'); ?></h3><?php endif; ?>
+															<?php the_sub_field('inset-text'); ?>
+														</div>
+													</div>
+
+												<?php elseif(get_sub_field('layout') == "inset-right") : ?>
+														<div class="inset-container">
+															<div class="media inset right">
+																<img src="<?php the_sub_field("image"); ?>" alt="" class="<?php if( get_sub_field('drop-shadow')){ ?>drop-shadow<?php } ?>"/>
+																<div class="caption"><?php the_sub_field("image-caption"); ?></div>
+															</div>
+															<div class="inset-text">
+																<?php if(get_sub_field('section-title')): ?><h3><?php the_sub_field('section-title'); ?></h3><?php endif; ?>
+																<?php the_sub_field('inset-text'); ?>
+															</div>
+														</div>
+
+												<?php elseif(get_sub_field('layout') == "inset-center") : ?>
+													<div class="media inset-center<?php if( get_sub_field('bkgd-color-change')){ ?> alt<?php } ?>">
+														<img src="<?php the_sub_field("image"); ?>" alt="" class="<?php if( get_sub_field('drop-shadow')){ ?>drop-shadow<?php } ?>"/>
+														<div class="caption"><?php the_sub_field("image-caption"); ?></div>
+													</div>
+
+												<?php elseif(get_sub_field('layout') == "outset") : ?>
+													<div class="media outset<?php if( get_sub_field('bkgd-color-change')){ ?> alt" style="background-color:<?php the_sub_field('custom-color'); ?><?php } ?>">
+														<img src="<?php the_sub_field("image"); ?>" alt="" class="<?php if( get_sub_field('drop-shadow')){ ?>drop-shadow<?php } ?>"/>
+														<div class="caption"><?php the_sub_field("image-caption"); ?></div>
+													</div>
+
+												<?php elseif(get_sub_field('layout') == "fill-width") : ?>
+													<div class="media fill-width" style="background-image: url(<?php the_sub_field("image"); ?>)";>
+														<div class="caption"><?php the_sub_field("image-caption"); ?></div>
+													</div>
+
+												<?php endif; ?>
+
+											<?php elseif( get_row_layout() == 'video' ): ?>
+
+												<div class="video-wrap<?php if( get_sub_field('bkgd-color-change')){ ?> alt-pool<?php } ?>">
+													<div class="media">
+
+														<div class="embed-container" style="padding-bottom:<?php the_sub_field('padding'); ?>%;">
+														<?php
+
+														// get iframe HTML
+														$iframe = get_sub_field('video');
+
+														// use preg_match to find iframe src
+														preg_match('/src="(.+?)"/', $iframe, $matches);
+														$src = $matches[1];
+														if (get_sub_field('autoplay')){
+															$params = array(
+													    'title'    => 0,
+													    'portrait'    => 0,
+													    'byline'    => 0,
+													    'autoplay'    => 1,
+													    'loop'    => 1,
+													    'color'    => 'ffffff',
+															'autopause'			=> 0
+														);
+														}
+
+														else {
+														// add extra params to iframe src
+														$params = array(
+													    'title'    => 0,
+													    'portrait'    => 0,
+													    'byline'    => 0,
+															'color'    => 'ffffff',
+															'autopause'			=> 0
+														);
+														}
+
+														$new_src = add_query_arg($params, $src);
+
+														$iframe = str_replace($src, $new_src, $iframe);
+
+														// add extra attributes to iframe html
+														$attributes = 'frameborder="0"';
+
+														$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+														// echo $iframe
+														echo $iframe;
+
+														?>
+														</div>
+
+														<div class="caption"><?php the_sub_field("image-caption"); ?></div>
+
+							        		</div>
+
+												</div>
+
+									<?php elseif( get_row_layout() == 'gallery' ): ?>
+
+										<?php
+
+										$images = get_sub_field('gallery');
+
+										if( $images ): ?>
+										    <div class="media">
+
+											    <div id="slider" class="flexslider <?php if( get_sub_field('drop-shadow')){ ?>drop-shadow-slider<?php } ?>">
+
+											        <ul class="slides">
+											            <?php foreach( $images as $image ): ?>
+											                <li>
+											                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+											                </li>
+											            <?php endforeach; ?>
+											        </ul>
+											    </div>
+											    <div class="caption"><?php the_sub_field("image-caption"); ?></div>
+										    </div>
+
+										<?php endif; ?>
+
+							        <?php endif;
+
+							    endwhile;
+
+							else :
+
+							    // no layouts found
+
+							endif; ?>
+
+						</section> <?php // end article section ?>
+
+					    <?php endwhile;
+
+					else :
+
+					    // no rows found
+
+					endif;
+
+					?>
+
+
+
+              </article> <?php // end article ?>
+
+
+
+			<?php endwhile; ?>
+
+			<?php else : ?>
+
+				<article id="post-not-found" class="hentry cf">
+						<header class="article-header">
+							<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+						</header>
+						<section class="entry-content">
+							<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+						</section>
+				</article>
+
+			<?php endif; ?>
+
+
+
+		</div>
+
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
