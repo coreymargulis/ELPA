661759350a340f0d88ea2a71398ce8776e6dc6dc
Corey Margulis <coreymargulis@gmail.com>
2016-03-12 10:33:51 -0600

    single members text styling. added website and email.

--- wp-content/themes/elpa/single-members.php
+++ wp-content/themes/elpa/single-members.php
@@ -13,7 +13,9 @@
 				<div class="image-gallery">Member artwork goes here.</div>
 				<div id="main" class="wrap">
 					<h1 id="page-title"><?php the_title(); ?></h1>
-					<div id="bio">Bio goes here<?php the_field('bio'); ?></div>
+					<div><a href="<?php the_field('website'); ?>">Website</a></div>
+					<div><a href="mailto:<?php the_field('email'); ?>">Contact Artist</a></div>
+					<div id="bio"><?php the_field('bio'); ?></div>
 				</div>
 
 	</div>
